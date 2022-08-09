ca7e2d5d4610642b363569788f591ddef0ec9b31
Levi <levialmeida02@outlook.com>
2022-08-09 17:58:06 -0300

    up

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Header } from "./components/Header"
+
+import styles from  './App.module.css'
+import './global.css'
+import { Sidebar } from "./components/Sidebar"
+
+function App() {
+  return (
+    <>
+    <Header />
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+
+      </main>
+    </div>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar( ) {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1607799279861-4dd421887fb3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDF8fGRldmVsb3BlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=300&q=50" alt="" />
+        
+            <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/LeviAlmeid.png" alt="" />
+
+                <strong>Levi Almeida</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">Editar seu perfil</a>
+            </footer>
+        </aside>
+    )
+}
